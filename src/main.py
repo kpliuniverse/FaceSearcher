4b74f9e27f1cda6d32be52bca18e5406e2cf046e
kpliuniverse <120769397+kpliuniverse@users.noreply.github.com>
2026-02-25 21:48:58 +0800

    now works

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import pathlib
 
 import PIL
 import cv2
@@ -23,14 +24,18 @@ def detect_faces(image: cv2.Mat):
 def ssim_two_images(image1: Image, image2: Image):
     gray1 = image1.convert("L")
     gray2 = image2.convert("L")
-    return ssim(np.array(gray1), np.array(gray2))
+    return ssim(np.array(gray1), np.array(gray2), gaussian_weights=True, sigma=1.5, use_sample_covariance=False, data_range=255)
 
 def compare_images(image1: cv2.Mat, image2: cv2.Mat):
     image1 = Image.fromarray(cv2.cvtColor(image1, cv2.COLOR_BGR2RGB))
     image2 = Image.fromarray(cv2.cvtColor(image2, cv2.COLOR_BGR2RGB))
-    print(image1.size, image2.size)
+    w = min(image1.size[0], image2.size[0])
+    h = min(image1.size[1], image2.size[1])
+    image1 = image1.resize((w, h))
+    image2 = image2.resize((w, h))
 
-    return image1, image2
+    return ssim_two_images(image1, image2)
+
 def main(args):
     image = cv2.imread(args[1])
     faces = detect_faces(image)
@@ -43,12 +48,19 @@ def main(args):
 
     x_source, y_source, w_source, h_source = face
     cropped_source_image = image[y_source:y_source+h_source, x_source:x_source+w_source]
+    similiarity_data = dict()
+
     for image_path in (os.listdir("data/")):
-        os.path.join("data/", image_path)
+        dest_path = pathlib.Path(os.path.join("data/", image_path))
+        
         
         logging.info(f"Comparing with {image_path}...")
-        dest_image = cv2.imread(os.path.join("data/", image_path))
-        for dest_face in detect_faces(dest_image):
+        dest_image = cv2.imread(dest_path)
+        dest_faces = detect_faces(dest_image)
+        if len(dest_faces) == 0:
+            logging.info(f"No faces detected at destination image {image_path}, skipping.")
+            continue
+        for dest_face in dest_faces:
             x_dest, y_dest, w_dest, h_dest = dest_face
             cropped_dest_image = dest_image[y_dest:y_dest+h_dest, x_dest:x_dest+w_dest]
-            similarity = compare_images(cropped_source_image, cropped_dest_image)
+            compare_images(cropped_source_image, cropped_dest_image)
