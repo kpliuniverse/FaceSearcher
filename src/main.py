304618f0d2c2473da32e0c8681fdc6fbf352779f
kpliuniverse <120769397+kpliuniverse@users.noreply.github.com>
2026-02-27 17:34:23 +0800

    grammar

--- src/main.py
+++ src/main.py
@@ -129,7 +129,7 @@ def show_result(data):
 def main(args):
     parser = argparse.ArgumentParser(prog="FaceSearcher", description="Compare faces in source image with a set of images in the destination folder.")
     parser.add_argument("-s", "--source_image", help="Path to the source image.")
-    parser.add_argument("-c", "--comparison_folder", help="Path to the comparison folder. Nesting supported")
+    parser.add_argument("-c", "--comparison_folder", help="Path to the comparison folder. Nesting supported.")
     parser.add_argument("--threshold", help="Similarity threshold, between 0 and 1, default is 0.3.", default=0.3, type=float)
     args = parser.parse_args(args[1:])
     dest_path = pathlib.Path(args.comparison_folder)
